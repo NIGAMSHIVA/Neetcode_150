45556c98884cf26140d3755782f55a3538cc7613
NIGAMSHIVA <210105056@hbtu.ac.in>
2025-05-18 12:14:06 +0530

    valid Anagram Solved

--- Arrays_Hashing/GroupAnagrams.java
+++ Arrays_Hashing/GroupAnagrams.java
@@ -8,28 +8,70 @@ public class GroupAnagrams {
 
     public static List<List<String>> groupAnagrams(String[] strs) {
 
-        List<String> l1 = new ArrayList<>();
+        // Approach 1 
 
+        // List<String> l1 = new ArrayList<>();
+
+        // List<List<String>> L1 = new ArrayList<>();
+
+        // HashMap<String, List<String>> map = new HashMap<>();
+
+        // for (int i = 0; i < strs.length; i++) {
+
+        //     char ch[] = strs[i].toCharArray();
+
+        //     Arrays.sort(ch); 
+
+        //     String sorted = new String(ch);
+
+        //     if (!map.containsKey(sorted)) {
+        //         map.put(sorted, new ArrayList<>()); 
+        //     }
+        //     map.get(sorted).add(strs[i]);
+        // }
+        
+        // L1.addAll(map.values());
+
+        // return L1;
+
+        // Approach 2
+         HashMap<String, List<String>> map = new HashMap<>();
         List<List<String>> L1 = new ArrayList<>();
-
-        HashMap<String, List<String>> map = new HashMap<>();
 
         for (int i = 0; i < strs.length; i++) {
 
+            int num[] = new int[26];
+
             char ch[] = strs[i].toCharArray();
+            // Not sorting instead I will generate a key
 
-            Arrays.sort(ch); 
-
-            String sorted = new String(ch);
-
-            if (!map.containsKey(sorted)) {
-                map.put(sorted, new ArrayList<>()); 
+            for (int j = 0; j < ch.length; j++) {
+                num[ch[j] - 'a']++;
             }
-            map.get(sorted).add(strs[i]);
+            // Now this num is an array I want something String type
+            // [1,0,0,0,0,0,0,1,0,0,0,0,0,0,0,1]
+            StringBuilder sb = new StringBuilder();
+
+            for (int z = 0; z < num.length; z++) {
+                sb.append("#"); // when b are 10 and d is 1 so these b -> 10 will give 1 0 type thing.
+                sb.append(num[z]);
+            }
+            // unique key is generated and stored in sb
+
+            String dummy = sb.toString();
+
+            if (!map.containsKey(dummy)) {
+
+                map.put(dummy, new ArrayList<>());
+            }
+
+            map.get(dummy).add(strs[i]);
         }
         
-        L1.addAll(map.values());
+        System.out.println(map);
 
+        L1.addAll(map.values());
+        
         return L1;
 
 
